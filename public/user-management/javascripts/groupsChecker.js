f550ca59988f65c70e6912a7d320d4f080d4628a
Estebanc PAMOE <estebanc.pamoe@enkoeducation.com>
2023-02-02 23:01:49 +0100

    school groups hidden for non choosen organization

--- public/user-management/javascripts/groupsChecker.js
+++ public/user-management/javascripts/groupsChecker.js
@@ -14,6 +14,7 @@ function groupChecker() {
     for (var i = 0; i < common.length; i++) {
         common[i].checked = true;
     }
+
     //Then we check the targets school
     var school = document.getElementById('school');
     var schoolCode = school.options[school.selectedIndex].value;
@@ -22,4 +23,29 @@ function groupChecker() {
         schoolTarget[i].checked = true;
     }
 
+    //And we hide the other school groups lines
+    hiddeNotSelectedSchoolGroups(schoolCode)
+}
+
+
+//function to hidde allschools groups not selected
+function hiddeNotSelectedSchoolGroups(school_code) {
+    document.querySelector(".AGP_groups").setAttribute("hidden", "true");
+    document.querySelector(".EBK_groups").setAttribute("hidden", "true");
+    document.querySelector(".EBC_groups").setAttribute("hidden", "true");
+    document.querySelector(".EBT_groups").setAttribute("hidden", "true");
+    document.querySelector(".KLS_groups").setAttribute("hidden", "true");
+    document.querySelector(".EKG_groups").setAttribute("hidden", "true");
+    document.querySelector(".KDS_groups").setAttribute("hidden", "true");
+    document.querySelector(".EGC_groups").setAttribute("hidden", "true");
+    document.querySelector(".EOG_groups").setAttribute("hidden", "true");
+    document.querySelector(".EVS_groups").setAttribute("hidden", "true");
+    document.querySelector(".ERV_groups").setAttribute("hidden", "true");
+    document.querySelector(".ESC_groups").setAttribute("hidden", "true");
+    document.querySelector(".EWC_groups").setAttribute("hidden", "true");
+    document.querySelector(".PED_groups").setAttribute("hidden", "true");
+
+    console.log("." + school_code + "_groups")
+        //Then display the choosen section
+    document.querySelector("." + school_code + "_groups").removeAttribute('hidden')
 }
