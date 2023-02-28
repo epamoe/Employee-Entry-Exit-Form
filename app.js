d7cc7ae25218c0a05c8bc6d5fb9b17930973e853
Estebanc PAMOE <estebanc.pamoe@enkoeducation.com>
2023-02-28 12:18:45 +0000

    https-404-error handled

--- app.js
+++ app.js
@@ -192,7 +192,10 @@ app.use(function(err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render(
+	'entryform', {
+        session: req.session,
+    });
 });
 
 module.exports = app;
